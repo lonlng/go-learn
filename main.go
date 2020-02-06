037d4fc0a9fef6b3e6bb857df328f706235a8b6b
lonlng <lonlng@163.com>
2020-02-06 15:53:24 +0800

    add mod

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	_ "net/http"
+
+	"github.com/lonlng/go-learn/abc"
+
+	_ "github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("hello world!")
+	abc.Abc()
+
 }
